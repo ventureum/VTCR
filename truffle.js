5d4b4f45db69585eef1610068ca8a1a35a67cea9
Timothy Wang <timothywangdev@gmail.com>
2018-04-30 06:37:29 +0000

    Add geth network for better testing environment

--- truffle.js
+++ truffle.js
@@ -1,12 +1,18 @@
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // for more about customizing your Truffle configuration!
-    networks: {
-        development: {
-            host: "ganache.ventureum.io",
-            port: 8545,
-            network_id: "*", // Match any network id
-            gas: 5000000
-        }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 5000000
+    },
+    geth: {
+      host: 'geth.ventureum.io',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 5000000
     }
-};
+  }
+}
